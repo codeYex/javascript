20b01c7ee6265cbaee6da2e1d91cdfc0ca2a150d
Code by Yex <yextheprogrammer@gmail.com>
2025-01-14 19:48:29 +0300

    made some changes

--- read.js
+++ read.js
@@ -1,1 +1,11 @@
 console.log(5);
+console.log (4 + 4); //addition of 4 and 4
+console.log(32); //logs 32 on console
+
+console.log(/*ADDITION OF TWO NUMBERS */ 4 + 10);
+
+/* 
+    This is a multi-line comment
+    that spans multiple lines
+    this will cater for all the codes inside the program
+*/
