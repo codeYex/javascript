7f2a3527b1cbfa27eda848c28370bbcb1bfacdae
Code by Yex <yextheprogrammer@gmail.com>
2025-01-15 22:11:37 +0300

    modified the variables file

--- variables.js
+++ variables.js
@@ -1,7 +1,33 @@
 
 
-'use strict';
-let user = "Micheal";
-let message = "Hello ";
 
-console.log(message ,user ); //will output the name with a greeting
+let user = "Micheal";
+let car = 'Toyota'; 
+let message = "Hello ";
+let list = user;
+let age = 42;
+console.log(message ,user, list , age); //will output the name with a greeting
+
+//CONSTANTS are unchanged variables
+const myDateOfBirth = '21/12/1990';
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLACK = "#000";
+
+let color = COLOR_RED;
+console.log(color) ;
+
+const pageLoadTime = 32 ; 
+
+
+let name = 'John';
+let admin = name;
+console.log(admin); // John
+
+
+let namePolice , administrator;
+namePolice = 'John';
+administrator = namePolice;
+
+console.log(administrator);
