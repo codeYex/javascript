0da6e21ae3ee4f48b9ea99614b781be5a7bcd167
Code by Yex <yextheprogrammer@gmail.com>
2025-01-15 20:36:20 +0300

    some new files

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Hello world');
+// console.log("Hello world");
+"use strict";
+
+console.log(3 + 2 + 3); // will output 8
---
+++ variables.js
@@ -0,0 +1,7 @@
+
+
+'use strict';
+let user = "Micheal";
+let message = "Hello ";
+
+console.log(message ,user ); //will output the name with a greeting
